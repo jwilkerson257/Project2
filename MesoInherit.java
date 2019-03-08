b65fb19d8706c7e8b13be607845c0e003557c9e7
jwilkerson257 <47432903+jwilkerson257@users.noreply.github.com>
2019-03-07 23:37:14 -0600

    Added comments to calAverage to enhance readability

--- MesoInherit.java
+++ MesoInherit.java
@@ -15,13 +15,22 @@ public class MesoInherit extends MesoAbstract
 		// TODO
 		double sum = 0;
 		int[] nodes = new int[3];
-
+		
+		// The for loop gets the ASCII values of each of the characters in the string mesoStation and adds them to the sum double variable
 		for (int k = 0; k < STID.getStID().length(); k++)
 		{
 			sum += (double) STID.getStID().charAt(k);
 		}
+		
+		// Takes the ceiling of the average ASCII values
 		nodes[0] = (int) Math.ceil(sum / STID.getStID().length());
+		
+		// Takes the floor of the average ASCII values
 		nodes[1] = (int) Math.floor(sum / STID.getStID().length());
+		
+		/* This if statement makes sure that if the decimal of the average of the StID values is greater than or equal to .5 
+		 * then it takes the ceiling of the value and the else statement takes the floor of the value if it is less than .5.
+		 */
 		if ((Math.ceil(sum / STID.getStID().length())) - (sum / STID.getStID().length()) >= .5)
 		{
 			nodes[2] = (int) Math.ceil(sum / STID.getStID().length());
@@ -31,6 +40,8 @@ public class MesoInherit extends MesoAbstract
 		{
 			nodes[2] = (int) Math.floor(sum / STID.getStID().length());
 		}
+		
+		// Sets the value of the average held in nodes[2] and assigns it to a global variable to use it in the letterAverage method
 		average = nodes[0];
 		return nodes;
 	}
@@ -42,4 +53,5 @@ public class MesoInherit extends MesoAbstract
 		// TODO
 		return (char) average;
 	}
+	
 }

262174395397c10df978e7bafa0e4577f6c738a0
jwilkerson257 <47432903+jwilkerson257@users.noreply.github.com>
2019-03-07 23:07:53 -0600

    Added comments to enhance readability

--- MesoAbstract.java
+++ MesoAbstract.java
@@ -1,6 +1,7 @@
 public abstract class MesoAbstract 
 {
 	//TODO
+	// The following methods are abstract in order to ensure the implementation of them in the inheriting class
 	abstract int[] calAverage();
 	abstract char letterAverage();
 }
